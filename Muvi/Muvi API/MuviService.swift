e5cdecec2879d76303d88c453f948eb3442ff0c0
Hasan Güngör <gungorhasan93@gmail.com>
2021-07-24 15:19:40 +0300

    xcconfig file added

--- Muvi/Muvi API/MuviService.swift
+++ Muvi/Muvi API/MuviService.swift
@@ -9,7 +9,11 @@ import Foundation
 import Alamofire
 
 class MuviService {
- 
+    
+    private var apiKey: String {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else { return "" }
+        return key
+    }
     static let shared = MuviService()
     private init() {}
 }
