6eec265a37b7b9ffe284e805e4abc229a218d300
Hasan Güngör <gungorhasan93@gmail.com>
2021-07-24 15:02:25 +0300

    MuviService and MuviRepository classes have added

---
+++ Muvi/Muvi API/MuviService.swift
@@ -0,0 +1,15 @@
+//
+//  MuviService.swift
+//  Muvi
+//
+//  Created by HasanEkmob on 19.07.2021.
+//
+
+import Foundation
+import Alamofire
+
+class MuviService {
+ 
+    static let shared = MuviService()
+    private init() {}
+}
---
+++ Muvi/Repository/MuviRepository.swift
@@ -0,0 +1,12 @@
+//
+//  MuviRepository.swift
+//  Muvi
+//
+//  Created by HasanEkmob on 19.07.2021.
+//
+
+import Foundation
+
+class MuviRepository {
+    
+}
